57c7d61ad2b5c203d72f16d1e5ca33d95aa7a678
matsumoto <haruka.m.66410@gmail.com>
2024-09-07 12:23:55 +0000

    Modify mypage controller

--- sound_score_app/app/controllers/mypage_controller.rb
+++ sound_score_app/app/controllers/mypage_controller.rb
@@ -1,5 +1,17 @@
 class MypageController < ApplicationController
+  # ユーザーがログインしていない場合、ログインページにリダイレクトする
+  before_action :authenticate_user!
+  before_action :correct_user
+
   def index
-    @history = current_user.histories
+    # Kaminariを使用して、履歴を15件ずつ取得し、ページネーションを実装
+    @histories = current_user.histories.order(played_at: :desc).page(params[:page]).per(15)
   end
+
+    private
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path, status: :see_other) unless @user == current_user
+    end
+  
 end
