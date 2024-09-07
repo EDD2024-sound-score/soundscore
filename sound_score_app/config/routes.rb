38b299eb2312df03eb1953fca7a93b5224c49161
Hideto1008 <137256949+Hideto1008@users.noreply.github.com>
2024-09-07 08:25:49 +0000

    Modify song_selection_controller

--- sound_score_app/config/routes.rb
+++ sound_score_app/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  root "song_selection#index"
+  root "song_selection#index"       # 楽曲選択画面
+  get 'song_selection/upload'       # 自分の楽曲をアップロードする画面
+  post 'song_selection/upload_song' # 楽曲をアップロードする処理
+  get 'song_selection/result'       # 精度の結果を出力する画面
 
   devise_for :users, controllers: {
     registrations: 'users/registrations',
